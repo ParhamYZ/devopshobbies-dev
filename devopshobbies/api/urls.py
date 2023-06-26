cbd438fd3d24da55101bac85982d7f7efc8580ed
parhamyzdn <parham@greenhabitat.at>
2023-06-26 22:04:37 +0330

    add auth apis

--- devopshobbies/api/urls.py
+++ devopshobbies/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-     path('blog/', include(('devopshobbies.blog.urls', 'blog')))
+     path('blog/', include(('devopshobbies.blog.urls', 'blog'))),
+     path('users/', include(('devopshobbies.users.urls', 'users'))),
+     path('auth/', include(('devopshobbies.authentication.urls', 'auth'))),
 ]
