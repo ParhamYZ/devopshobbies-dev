782a314de0a85f4b9a3bce4419351fef7dacc297
parhamyzdn <parham@greenhabitat.at>
2023-06-28 02:23:38 +0330

    add selectors and services

--- devopshobbies/blog/services/products.py
+++
@@ -1,5 +0,0 @@
-from django.db.models import QuerySet
-from devopshobbies.blog.models import Product
-
-def create_product(name: str) -> QuerySet[Product]:
-    return Product.objects.create(name=name)
