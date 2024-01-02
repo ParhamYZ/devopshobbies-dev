a8f5d5dd91812d12fd05f9ee2af3d5d4d9d3ddf5
ParhamYZ <43354577+ParhamYZ@users.noreply.github.com>
2024-01-02 12:23:31 +0330

    Update base.py
    fix secret key

--- config/django/base.py
+++ config/django/base.py
@@ -4,7 +4,7 @@ from config.env import env, BASE_DIR
 env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=ug_ucl@yi6^mrcjyz%(u0%&g2adt#bz3@yos%#@*t#t!ypx=a'
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
