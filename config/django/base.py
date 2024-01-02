659a472e72126987d3742dfff6393f83888bba05
ParhamYZ <43354577+ParhamYZ@users.noreply.github.com>
2024-01-02 12:53:16 +0330

    Update base.py

--- config/django/base.py
+++ config/django/base.py
@@ -4,7 +4,7 @@ from config.env import env, BASE_DIR
 env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY", default="")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
